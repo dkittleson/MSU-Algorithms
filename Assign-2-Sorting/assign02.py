6c7f238ccb97177ca2a62ec03ad788dde178875b
dkittleson <dkittles@msudenver.edu>
2022-06-15 07:46:05 -0600

    improved Mergesort 2 & 3 readability

--- Assign-2-Sorting/assign02.py
+++ Assign-2-Sorting/assign02.py
@@ -1,3 +1,4 @@
+from queue import Empty
 import time
 import random
 import sys
@@ -41,7 +42,7 @@ def swap2(list_swap, from_idx, to_idx):
     list_swap[to_idx] = temp
 
 
-def mergeSort(list_of_items, split_by_3=False):
+def mergeSort(list_items, split_by_3=False):
     """Checks if splitting by 2 or 3 and returns sorted
     Source:
     problem solving with algorithms and data structures using python 3rd ed.
@@ -49,231 +50,158 @@ def mergeSort(list_of_items, split_by_3=False):
     """
     start_time = time.time()
     if not split_by_3:
-        mergeSort2(list_of_items)
+        mergeSortTwo(list_items)
     if split_by_3:
-        mergeSort3(list_of_items)
+        mergeSort3(list_items)
     elapsed_time = time.time() - start_time
-    return (list_of_items, elapsed_time)
+    return (list_items, elapsed_time)
 
 
-def mergeSort2(list_of_items):
+def mergeSortTwo(list_items):
     """This function splits list_of_items recurively.
     Then returns each sublist recursively and iteratively
     sorts.
     """
-    if len(list_of_items) > 1:
-        if len(list_of_items) % 2 == 0:
-            mid = len(list_of_items) // 2
-        else:
-            mid = (len(list_of_items) + 1) // 2
-        low_half = list_of_items[:mid]
-        high_half = list_of_items[mid:]
+    if len(list_items) > 1:
+        m = mergeSplitTwo(len(list_items))
+        list_lo = list_items[:m]
+        list_hi = list_items[m:]
 
-        mergeSort2(low_half)
-        mergeSort2(high_half)
-
-        i, j, k = 0, 0, 0
-
-        while i < len(low_half) and j < len(high_half):
-            if low_half[i] < high_half[j]:
-                list_of_items[k] = low_half[i]
-                i += 1
+        mergeSortTwo(list_lo)
+        mergeSortTwo(list_hi)
+        lo, hi, i = 0, 0, 0
+        while lo < len(list_lo) and hi < len(list_hi):
+            if list_lo[lo] < list_hi[hi]:
+                list_items[i] = list_lo[lo]
+                lo += 1
             else:
-                list_of_items[k] = high_half[j]
-                j += 1
-            k += 1
-
-        while i < len(low_half):
-            list_of_items[k] = low_half[i]
+                list_items[i] = list_hi[hi]
+                hi += 1
             i += 1
-            k += 1
-
-        while j < len(high_half):
-            list_of_items[k] = high_half[j]
-            j += 1
-            k += 1
-    return list_of_items
+        list_items[i:] = list_lo[lo:] or list_hi[hi:]
+    return list_items
 
 
-def mergeSort3(list_of_items):
+def mergeSplitTwo(m):
+    if m % 2 == 0:
+        m = m // 2
+    else:
+        m = (m + 1) // 2
+    return m
+
+
+def mergeSort3(list_items):
     """Recursively splits list_of_items into thirds, while checking for an
     even or odd length. Then iteratively and recursively sorts
     """
-    if len(list_of_items) > 1:
-        if len(list_of_items) % 2 == 0:
-            split = (len(list_of_items) + 1) // 3
-        else:
-            split = len(list_of_items) // 3
-        low_list = list_of_items[:split]
-        mid_list = list_of_items[split:split * 2]
-        high_list = list_of_items[split * 2:]
+    if len(list_items) > 1:
+        split = mergeSplitThree(len(list_items))
+        list_lo = list_items[:split]
+        list_mid = list_items[split:split * 2]
+        list_hi = list_items[split * 2:]
 
-        mergeSort3(low_list)
-        mergeSort3(mid_list)
-        mergeSort3(high_list)
+        mergeSort3(list_lo)
+        mergeSort3(list_mid)
+        mergeSort3(list_hi)
 
-        i, k, j, g = 0, 0, 0, 0
-        # NEEDS ONE TO SORT FULLY
-        while i < len(low_list) and j < len(high_list) and g < len(mid_list):
-            #  Low and Mid
-            if low_list[i] < high_list[j] and mid_list[g] < high_list[j]:
-                if low_list[i] < mid_list[g]:
-                    list_of_items[k] = low_list[i]
-                    i += 1
-                elif low_list[i] > mid_list[g]:
-                    list_of_items[k] = mid_list[g]
-                    g += 1
-                k += 1
-
-            #  Mid and High
-            elif mid_list[g] < low_list[i] and high_list[j] < low_list[i]:
-                if high_list[j] < mid_list[g]:
-                    list_of_items[k] = high_list[j]
-                    j += 1
-                elif high_list[j] > mid_list[g]:
-                    list_of_items[k] = mid_list[g]
-                    g += 1
-                k += 1
-
-            #  High and Low
-            elif high_list[j] < mid_list[g] and low_list[i] < mid_list[g]:
-                if low_list[i] < high_list[j]:
-                    list_of_items[k] = low_list[i]
-                    i += 1
-                elif low_list[i] > high_list[j]:
-                    list_of_items[k] = high_list[j]
-                    j += 1
-                k += 1
-
-        # low and mid
-        while i < len(low_list) and g < len(mid_list):
-            if low_list[i] < mid_list[g]:
-                list_of_items[k] = low_list[i]
-                i += 1
-            elif low_list[i] > mid_list[g]:
-                list_of_items[k] = mid_list[g]
-                g += 1
-            k += 1
-
-        # low and high
-        while i < len(low_list) and j < len(high_list):
-            if low_list[i] < high_list[j]:
-                list_of_items[k] = low_list[i]
-                i += 1
-            elif low_list[i] > high_list[j]:
-                list_of_items[k] = high_list[j]
-                j += 1
-            k += 1
-
-        # Mid and High
-        while j < len(high_list) and g < len(mid_list):
-            if high_list[j] < mid_list[g]:
-                list_of_items[k] = high_list[j]
-                j += 1
-            elif high_list[j] > mid_list[g]:
-                list_of_items[k] = mid_list[g]
-                g += 1
-            k += 1
-
-        # just low
-        while i < len(low_list):
-            list_of_items[k] = low_list[i]
+        lo, mid, hi, i = 0, 0, 0, 0
+        while lo < len(list_lo) and hi < len(list_hi) and mid < len(list_mid):
+            list_items[i] = min(list_lo[lo], list_mid[mid], list_hi[hi])
+            if list_items[i] == list_lo[lo]:
+                lo += 1
+            elif list_items[i] == list_mid[mid]:
+                mid += 1
+            else:
+                hi += 1
             i += 1
-            k += 1
 
-        # just mid
-        while g < len(mid_list):
-            list_of_items[k] = mid_list[g]
-            g += 1
-            k += 1
+        while lo < len(list_lo) and mid < len(list_mid):
+            list_items[i] = min(list_lo[lo], list_mid[mid])
+            if list_items[i] == list_lo[lo]:
+                lo += 1
+            else:
+                mid += 1
+            i += 1
 
-        # just high
-        while j < len(high_list):
-            list_of_items[k] = high_list[j]
-            j += 1
-            k += 1
+        while lo < len(list_lo) and hi < len(list_hi):
+            list_items[i] = min(list_lo[lo], list_hi[hi])
+            if list_items[i] == list_lo[lo]:
+                lo += 1
+            else:
+                hi += 1
+            i += 1
 
-    return list_of_items
+        while mid < len(list_mid) and hi < len(list_hi):
+            list_items[i] = min(list_mid[mid], list_hi[hi])
+            if list_items[i] == list_mid[mid]:
+                mid += 1
+            else:
+                hi += 1
+            i += 1
+        list_items[i:] = list_lo[lo:] or list_mid[mid:] or list_hi[hi:]
+
+    return list_items
+
+
+def mergeSplitThree(split):
+    if split % 2 == 0:
+        split = (split + 1) // 3
+    else:
+        split = split // 3
+    return split
 
 
 def quickSort(list_of_items, pivot_to_use='first'):
-    """Determines if sorting by first value or middle value.
-    Source:
-    problem solving with algorithms and data structures using python 3rd ed.
-    pg 178 - 179
-    """
+    """Determines if sorting by first value or middle value."""
     start_time = time.time()
-    end_index = len(list_of_items) - 1
     if pivot_to_use == 'first':
-        quicksort_helper(list_of_items, 0, end_index, partition_first)
+        qs_first(list_of_items, 0, len(list_of_items))
     if pivot_to_use == 'middle':
-        quicksort_helper(list_of_items, 0, end_index, partition_mid)
+        qs_mid(list_of_items, 0, len(list_of_items))
     elapsed_time = time.time() - start_time
     return (list_of_items, elapsed_time)
 
 
-def quicksort_helper(list_of_items, low, high, partition_func):
-    """Used to recursively call partion, utilizing quicksort_helper
-    to use the first element as a split point
-    """
-    if low < high:
-        split = partition_func(list_of_items, low, high)
-        quicksort_helper(list_of_items, low, split - 1, partition_func)
-        quicksort_helper(list_of_items, split + 1, high, partition_func)
+def part_first(arr, lo, hi):
+    """[Insert summary]"""
+    i, p = lo + 1, arr[lo]
+    for j in range(i, hi):
+        if arr[j] < p:
+            arr[i], arr[j], i = arr[j], arr[i], i + 1
+    arr[lo], arr[i - 1] = arr[i - 1], arr[lo]
+    return i - 1
 
 
-def partition_first(list_of_items, low, high):
-    """Uses first value in list as pivot, the swaps pivot or values"""
-    pivot = list_of_items[low]
-    left_mark = low + 1
-    right_mark = high
-    while True:
-        while left_mark <= right_mark and list_of_items[left_mark] < pivot:
-            left_mark += 1
-        while left_mark <= right_mark and list_of_items[right_mark] > pivot:
-            right_mark -= 1
-        if left_mark >= right_mark:
-            # swap pivot to correct location
-            tmp = list_of_items[low]
-            list_of_items[low] = list_of_items[right_mark]
-            list_of_items[right_mark] = tmp
-
-            return right_mark
-
-        # swap the wrong locations
-        tmp = list_of_items[left_mark]
-        list_of_items[left_mark] = list_of_items[right_mark]
-        list_of_items[right_mark] = tmp
-
-        # left_mark += 1
-        # right_mark -= 1
+def qs_first(arr, lo, hi):
+    """[Insert summary]"""
+    if lo < hi:
+        p = part_first(arr, lo, hi)
+        qs_first(arr, lo, p), qs_first(arr, p + 1, hi)
 
 
-def partition_mid(list_of_items, low, high):
-    """Uses middle value as pivot, then swaps values"""
-    pivot = list_of_items[(low + high + 1) // 2]
-    left_mark = low
-    right_mark = high
-    while True:
-        while left_mark <= right_mark and list_of_items[left_mark] < pivot:
-            left_mark += 1
-        while left_mark <= right_mark and list_of_items[right_mark] > pivot:
-            right_mark -= 1
-        if left_mark >= right_mark:
-            return right_mark
+def qs_mid(arr, lo: int, hi: int):
+    """[Insert summary]"""
+    if (hi - lo) <= 1:
+        return
+    p = part_mid(arr, lo, hi)
+    qs_mid(arr, lo, p), qs_mid(arr, p + 1, hi)
 
-        # swap the wrong locations
-        tmp = list_of_items[left_mark]
-        list_of_items[left_mark] = list_of_items[right_mark]
-        list_of_items[right_mark] = tmp
+
+def part_mid(arr, lo, hi):
+    """[Insert summary]"""
+    arr[hi - 1], arr[(hi + lo - 1) // 2], i = arr[(hi + lo - 1) // 2],\
+        arr[hi - 1], lo
+    for j in range(lo, hi):
+        if arr[j] < arr[hi - 1]:
+            arr[i], arr[j], i = arr[j], arr[i], i + 1
+    arr[hi - 1], arr[i] = arr[i], arr[hi - 1]
+    return i
 
 
 def radixSort(list_of_items, max_digit):
     """Finds largest element in array and sorts the smallest digit
     to the largest digit in their respective arrays. Calls
     counts for each sub array. Adjusting while loop to fix time constrant.
-    Source:
-    alphacodingskills.com/python/pages/python-program-for-radix-sort.php
     """
     start_time = time.time()
     place = 1
@@ -309,7 +237,7 @@ def assign02_main():
     random.seed(1)
     random.shuffle(list1)
 
-    # list1 = [54, 26, 93, 17, 77, 31, 44, 55, 20, 11]
+    # list1 = [54, 26, 93, 17, 77, 31, 44, 55, 20, 11, 82, 45, 67, 9]
 
     # run sorting functions
     bubbleRes = bubbleSort(list(list1))
@@ -337,8 +265,6 @@ def assign02_main():
 
     # Try with a list sorted in reverse order (worst case for quicksort)
     list2 = list(range(6000, 1000, -1))
-
-    # list2 = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 
     # run sorting functions
     bubbleRes = bubbleSort(list(list2))
